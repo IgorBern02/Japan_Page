9fbc4bd9177877e50de670fd525389c68c5d6a69
IgorBern02 <igor.bernardes2@gmail.com>
2023-08-09 22:38:16 -0300

    .

---
+++ firstPage/script.js
@@ -0,0 +1,26 @@
+// Função Hover
+// function chanceImg(){
+//     document.getElementById('arrow-down').src = "./firstPage/images/arrow-up.png";
+// }
+
+// function backImg(){
+//     document.getElementById('arrow-down').src = "./firstPage/images/arrow-down.png";
+// }
+
+// // Slider mudando automatico
+
+// let count = 1;
+// document.getElementById('radio1').checked = true;
+    
+// setInterval( () => {
+//     nextImg();
+// }, 5000)
+
+// function nextImg(){
+//     count++;
+//     if(count>5){
+//         count = 1;
+//     }
+
+//     document.getElementById('radio'+count).checked = true;
+// }
